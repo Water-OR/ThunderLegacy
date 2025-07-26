46905c7c5c656cfb52e3ceb72fb42a16cc2f9d5c
Water-OR <a12300680@126.com>
2025-07-26 21:52:32 +0800

    improve MinecraftUtil

--- lib/src/main/kotlin/net/llvg/thunder/legacy/util/Minecraft.kt
+++ lib/src/main/kotlin/net/llvg/thunder/legacy/util/Minecraft.kt
@@ -2,7 +2,19 @@
 
 package net.llvg.thunder.legacy.util
 
+import kotlin.internal.InlineOnly
 import net.minecraft.client.Minecraft
+import net.minecraft.entity.Entity
 
 val mc: Minecraft
     get() = Minecraft.getMinecraft()
+
+@Suppress("UNUSED")
+@InlineOnly
+inline fun Entity.setRotation(
+    yaw: Float,
+    pitch: Float
+) {
+    rotationYaw = yaw
+    rotationPitch = pitch
+}
