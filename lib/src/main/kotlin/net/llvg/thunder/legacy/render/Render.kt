9302c0cda9885136f462212e77d204e4cb09b28f
Water-OR <a12300680@126.com>
2025-07-26 15:20:40 +0800

    improve RenderUtil

--- lib/src/main/kotlin/net/llvg/thunder/legacy/render/Render.kt
+++ lib/src/main/kotlin/net/llvg/thunder/legacy/render/Render.kt
@@ -3,7 +3,20 @@
 package net.llvg.thunder.legacy.render
 
 import kotlin.internal.InlineOnly
+import net.minecraft.client.gui.FontRenderer
 
 @InlineOnly
 @Suppress("UNUSED")
 inline fun String.compileAsShader(type: GLShaderType) = GLShader(type, this)
+
+@InlineOnly
+@JvmOverloads
+@Suppress("UNUSED")
+inline fun FontRenderer.drawCenteredString(
+    text: String,
+    x: Float,
+    y: Float,
+    color: Int,
+    dropShadow: Boolean = false
+): Int =
+    drawString(text, x - getStringWidth(text) / 2f, y, color, dropShadow)
